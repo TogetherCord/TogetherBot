4856cb64db7f1cf0df694be234160f65685c2ef6
Unknown-user-dev <jumperkylian@outlook.com>
2023-11-15 21:48:04 +0100

    fix(bot): add class commands

--- Loaders/loadCommands.js
+++ Loaders/loadCommands.js
@@ -1,12 +1,37 @@
-const fs = require("fs")
+const fs = require("fs");
+const path = require("path");
 
 module.exports = async bot => {
+    const commandFiles = getAllCommandFiles("./Commands");
 
-    fs.readdirSync("./Commands").filter(f => f.endsWith(".js")).forEach(async file => {
+    for (const file of commandFiles) {
+        const command = require(file);
+        if (!command.name || typeof command.name !== "string") {
+            throw new TypeError(`La commande ${path.basename(file, ".js")} n'a pas de nom !`);
+        }
+        bot.commands.set(command.name, command);
+    }
+};
 
-        let command = require(`../Commands/${file}`)
-        if(!command.name || typeof command.name !== "string") throw new TypeError(`La commande ${file.slice(0, file.length - 3)} n'a pas de nom !`)
-        bot.commands.set(command.name, command)
+function getAllCommandFiles(directory) {
+    const commandFiles = [];
 
-    })
+    function readDirectory(dir) {
+        const files = fs.readdirSync(dir);
+
+        files.forEach(file => {
+            const filePath = path.join(dir, file);
+            const isDirectory = fs.statSync(filePath).isDirectory();
+
+            if (isDirectory) {
+                readDirectory(filePath);
+            } else if (file.endsWith(".js")) {
+                commandFiles.push(path.resolve(filePath));
+            }
+        });
+    }
+
+    readDirectory(directory);
+    return commandFiles;
 }
+
