629f5982f927899fba7516ff8510cbabcd1b0b3f
Unknown User <jumperkylian@outlook.com>
2024-02-23 22:07:12 +0100

    feat(bot): cgu cinstance (need to fix a bug)

--- src/commands/slash/Instance/createinstance.js
+++ src/commands/slash/Instance/createinstance.js
@@ -4,7 +4,10 @@ const { ChatInputCommandInteraction, SlashCommandBuilder,
     ModalBuilder,
     ActionRowBuilder,
     TextInputBuilder,
-    TextInputStyle
+    TextInputStyle,
+    EmbedBuilder,
+    ButtonBuilder,
+    ButtonStyle, Embed
 } = require('discord.js');
 const ExtendedClient = require('../../../class/ExtendedClient');
 
@@ -20,6 +23,29 @@ module.exports = {
      * @param {ChatInputCommandInteraction} interaction
      */
     run: async (client, interaction) => {
+        const cguEmbed = new EmbedBuilder()
+            .setTitle('Terms of Service')
+            .setDescription('By creating an instance, you agree to the terms of service.')
+            .setColor('#FF0000')
+
+
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('accept')
+                    .setLabel('Accept')
+                    .setStyle(ButtonStyle.Success),
+                new ButtonBuilder()
+                    .setCustomId('decline')
+                    .setLabel('Decline')
+                    .setStyle(ButtonStyle.Danger)
+            );
+
+        await interaction.reply({
+            embeds: [cguEmbed],
+            components: [row]
+        });
+
         const modal = new ModalBuilder()
             .setTitle('Create your instance !')
             .setCustomId('instance-creator')
@@ -35,5 +61,15 @@ module.exports = {
                     )
             );
 
-        await interaction.showModal(modal);
+
+
+        client.once('interactionCreate', async (interaction) => {
+            if (!interaction.isButton()) return;
+            if (interaction.customId === 'accept') {
+                await interaction.showModal(modal);
+            } else if (interaction.customId === 'refuse') {
+                await interaction.deferReply('Vous avez refusé les conditions générales d\'utilisation.');
+            }
+        });
+
     }};
