777d667295af29f1b27dce9c0fdb1f9de880a524
Unknown User <jumperkylian@outlook.com>
2024-02-24 17:09:06 +0100

    feat(bot): Create a owner.js

--- src/commands/slash/Developers/owner.js
+++ src/commands/slash/Developers/owner.js
@@ -11,133 +11,82 @@ const {
     TextInputStyle, StringSelectMenuOptionBuilder,
     AttachmentBuilder
 } = require('discord.js');
-const ExtendedClient = require('../../../class/ExtendedClient');
-
-let intervalId;
+const ExtendedClient = require("../../../class/ExtendedClient");
+const GetInstanceNumber = require("../../../class/Request/GetInstanceNumber");
+const GetConnected = require("../../../class/Request/GetConnected");
 
 module.exports = {
     structure: new SlashCommandBuilder()
-        .setName('owner')
-        .setDescription('Owner Dashboard'),
+        .setName("owner")
+        .setDescription("A owner dashboard for TogetherCord"),
     /**
      * @param {ExtendedClient} client
      * @param {ChatInputCommandInteraction} interaction
      */
-    options: {
-        developers: true,
-    },
+
     run: async (client, interaction) => {
-
-        const row1 = new ActionRowBuilder()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('update')
-                    .setLabel('Update')
-                    .setEmoji('1190463492759756940')
-                    .setStyle(ButtonStyle.Success),
-                new ButtonBuilder()
-                    .setCustomId('restart')
-                    .setLabel('Restart')
-                    .setEmoji('1190464803140030615')
-                    .setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-                    .setCustomId('users')
-                    .setLabel('Users')
-                    .setEmoji('1190463990518779984')
-                    .setStyle(ButtonStyle.Secondary),
-            );
-
-        await interaction.deferReply({ ephemeral: true });
-
-        var embed = new EmbedBuilder()
-            .setColor('#0042ff')
-            .setTitle('Owner')
-            .setDescription('hmmmm')
-        embed.setThumbnail(client.user.displayAvatarURL({dynamic: true}));
-
-        await interaction.user.send({embeds: [embed], components: [row1]});
-        await interaction.editReply({ content: 'Check your dms !'});
-
-        client.on('interactionCreate', async (interaction) => {
-            if (!interaction.isButton() && !interaction.isStringSelectMenu()) return;
-
-            if (interaction.replied || interaction.deferred) return;
-
-            try {
-                await interaction.deferReply();
-            } catch (error) {
-                console.error('Error:', error);
+        try {
+            await interaction.deferReply({ ephemeral: true});
+            const developerid = require("../../../config.js").users.developers;
+            if (!developerid.includes(interaction.user.id)) {
+                await interaction.editReply({
+                    content: "You are not authorized to use this command",
+                    ephemeral: true,
+                });
                 return;
             }
+        } catch (error) {
+            console.error(error);
+            return;
+        }
 
-            if (interaction.isButton()) {
-                switch (interaction.customId) {
+        const row1 = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setCustomId("update")
+                .setLabel("Update")
+                .setEmoji("1190143453250732052")
+                .setStyle(ButtonStyle.Success),
+            new ButtonBuilder()
+                .setCustomId("restart")
+                .setLabel("Restart All")
+                .setEmoji("1190143455024922704")
+                .setStyle(ButtonStyle.Secondary),
+            new ButtonBuilder()
+                .setCustomId("users")
+                .setLabel("Users")
+                .setEmoji("1190143456346116126")
+                .setStyle(ButtonStyle.Danger),
+        );
 
-                case 'users':
+        const instanceNumber = await GetInstanceNumber()
 
-                    var embed = new EmbedBuilder()
-                        .setColor('#0042ff')
-                        .setTitle('Users')
-                        .setDescription('Every users')
-                    embed.setThumbnail(client.user.displayAvatarURL({ dynamic: true }));
+        const connectedcall = await GetConnected()
+        const connected = connectedcall.data.length
 
-                    var message = await interaction.editReply({ embeds: [embed]});
-                    setTimeout(() => {
-                        message.delete().catch(console.error);
-                    }, 20000);
-                    break;
+        var embed = new EmbedBuilder()
+            .setTitle("Owner Dashboard")
+            .setDescription("Welcome to the owner dashboard. Here you can update, restart or view users.")
+            .setColor("#00ffd8")
+            .addFields({
+                name: "<:RamUsage:1189573578895663204> How many instances are created :",
+                value: `**__${instanceNumber} Instances__**`,
+                inline: true
+            })
+            .addFields({
+                name: "<:Status:1189573575506677781> How many users are connected :",
+                value: `**__${connected} Users__**`,
+                inline: true
+            })
+        embed.setThumbnail(client.user.displayAvatarURL({dynamic: true}))
 
-                    case 'update':
-                  /*
-                    let formData = new FormData()
+        await interaction.user.send({
+            embeds: [embed],
+            components: [row1]
+        });
 
-                    formData.append('update', file);
-
-                    fetch('http://localhost:3333/update/self', {
-                        method: 'POST',
-                        body: formData
-                    })
-                        .then(response => response.json())
-                        .then(result => {
-                        })
-                        .catch(error => {
-                        });
-
-
-*/
-                    var embed = new EmbedBuilder()
-                        .setColor('#0042ff')
-                        .setTitle('Updater - System TogetherCord')
-                        .setDescription('Veuillez envoyer le fichier à envoyer sur le serveur en répondant à ce message')
-                        embed.setThumbnail(client.user.displayAvatarURL({ dynamic: true }));
-
-                    var message = await interaction.editReply({ embeds: [embed]});
-
-
-                    const filter = m => m.author.id === interaction.user.id;
-
-                    const collector = interaction.channel.createMessageCollector({ filter, time: 15000 });
-
-
-
-
-                    default:
-                        await interaction.reply('Unknown button');
-                        break;
-                }
-            } else if (interaction.isStringSelectMenu()) {
-                const selectedOption = interaction.values[0];
-
-                switch (selectedOption) {
-
-                    default:
-                        var message = await interaction.editReply('Unknown option');
-                        setTimeout(() => {
-                            message.delete().catch(console.error);
-                        }, 5000);
-                        break;
-                }
-            }
-        })
-    }
-}
+        await interaction.editReply({
+            content: "Check your dms !",
+            ephemeral: true,
+        });
+    },
+};
